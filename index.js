f89fbf36807a11f08c80ebb27744fa67e5e33508
Gaby Karam <info@gabykaram.com>
2021-12-13 13:42:42 +0200

    Change the way of getting client IP address

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+var requestIp = require('request-ip');
 app.get('/', (req, res) => {
     // res.send('Hello World!')
     var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     console.log({
         ip
     })
-    res.send(ip)
+    var clientIp = requestIp.getClientIp(request);
+    console.log(clientIp);
+    res.send(clientIp)
     // fetch("https://ipinfo.io/json?token=3643b3284205a4").then(
     //     (response) => response.json()
     // ).then(
