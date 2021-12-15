58083c2e24502271103fd72b5c3331977fbaa591
Gaby Karam <info@gabykaram.com>
2021-12-15 18:04:30 +0200

    Adding Rotating Compass plus some content to the about page

--- MyMonty-eu-en-v2/mymonty/src/components/pages/about-web/about-web.tsx
+++ MyMonty-eu-en-v2/mymonty/src/components/pages/about-web/about-web.tsx
@@ -1,4 +1,4 @@
-import { Component, Host, h } from '@stencil/core';
+import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'about-web',
@@ -9,9 +9,10 @@ export class AboutWeb {
 
   render() {
     return (
-      <Host>
-        <slot></slot>
-      </Host>
+      <div class="wrapper">
+        <text-with-color></text-with-color>
+        {/* <contact-form></contact-form> */}
+      </div>
     );
   }
 
---
+++ MyMonty-eu-en-v2/mymonty/src/components/rotating-compass/readme.md
@@ -0,0 +1,23 @@
+# rotating-compass
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Dependencies
+
+### Used by
+
+ - [app-root](../app-root)
+
+### Graph
+```mermaid
+graph TD;
+  app-root --> rotating-compass
+  style rotating-compass fill:#f9f,stroke:#333,stroke-width:4px
+```
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
