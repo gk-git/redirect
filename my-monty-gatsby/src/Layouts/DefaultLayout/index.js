af4e6ce47e2ea7981f42a589dee47e29dbf98085
Kevin <kevinabouhanna@gmail.com>
2021-12-16 00:32:37 +0200

    commit

--- my-monty-gatsby/src/Layouts/DefaultLayout/index.js
+++ my-monty-gatsby/src/Layouts/DefaultLayout/index.js
@@ -1,18 +1,15 @@
-import React from 'react'
-import Footer from '../../components/Footer'
-import Header from '../../components/Header'
-import RotatingCompass from '../../components/RotatingCompass'
+import React from "react";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import RotatingCompass from "../../components/RotatingCompass";
 
-export default function DefaultLayout ({ children, baseUrl }) {
-  
+export default function DefaultLayout({ children, baseUrl }) {
   return (
     <>
-      <RotatingCompass/>
-      <Header baseUrl={baseUrl}/>
-      <main>
-        {children}
-      </main>
-      <Footer/>
+      <RotatingCompass />
+      <Header baseUrl={baseUrl} />
+      <main>{children}</main>
+      <Footer />
     </>
-  )
+  );
 }
